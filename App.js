2a0a0cc0d4084a7a6cec6d9bc9aea94aca038c61
Peter Irlam <irlampeter@gmail.com>
2020-09-07 11:21:57 +0100

    Create React App of doggy contact info

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="doggyContacts">
+			<div className="contactInfo">
+				<img src="http://placedog.net/300/200" />
+				<h3>Mr. Houndson</h3>
+				<p>Phone: (212) 555-2673</p>
+				<p>Email: houndsonr@ruffmail.com</p>
+			</div>
+
+			<div className="contactInfo">
+				<img src="http://placedog.net/400/200" />
+				<h3>Doggy McDogface</h3>
+				<p>Phone: (212) 555-1784</p>
+				<p>Email: dmdf@groomme.com</p>
+			</div>
+
+			<div className="contactInfo">
+				<img src="http://placedog.net/400/300" />
+				<h3>General</h3>
+				<p>Phone: (212) 555-1165</p>
+				<p>Email: gbarks@yahoo.com</p>
+			</div>
+
+			<div className="contactInfo">
+				<img src="http://placedog.net/200/100" />
+				<h3>Pantston Winthorpe III</h3>
+				<p>Phone: (212) 555-8530</p>
+				<p>Email: pwthethird@hotmail.com</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
